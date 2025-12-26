fd2c814f7bb465511c2328345ed6c1cdede012a9
Peter Gerdzhikov <peter.gerdzhikov.contact@gmail.com>
2025-12-26 22:19:23 +0200

    Adding listeners for websocket connection and disconnection

---
+++ src/main/java/com/ItCareerElevatorFifthExercise/util/WebSocketEventsListener.java
@@ -0,0 +1,36 @@
+package com.ItCareerElevatorFifthExercise.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.security.Principal;
+
+@Component
+@RequiredArgsConstructor
+public class WebSocketEventsListener {
+
+    @EventListener
+    public void onConnect(SessionConnectEvent event) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+        Principal principal = accessor.getUser();
+        String sessionId = accessor.getSessionId();
+
+        String username = principal.getName();
+        System.out.println("Connection from user " + username);
+    }
+
+    @EventListener
+    public void onDisconnect(SessionDisconnectEvent event) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+        Principal principal = accessor.getUser();
+        String sessionId = accessor.getSessionId();
+
+        String username = principal.getName();
+
+        System.out.println("User " + username + " disconnected.");
+    }
+}
